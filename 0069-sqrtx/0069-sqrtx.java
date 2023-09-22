281670eb1c3a3e8e3fb00e346b11452259f750ca
PUSHPENDU MUKHERJEE <98036693+MukherjeePushpendu@users.noreply.github.com>
2023-09-22 23:41:52 +0530

    Time: 1 ms (97.7%), Space: 39.3 MB (84.07%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,27 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0)
-        {
-            return 0;
-        }
-        if(x==1)
-        {
-            return 1;
-        }
-      
-       
-
-        for(int i=1;i<=x/2;i++)
-        {
-            if((long)i*i==x)
-            return (int)i;
-            else if((long)i*i>x)
-            {
-                return (int)i-1;
-            }
-
-
-        }
-        return x/2;
+        double b=(double)x;
+        b=Math.floor(Math.sqrt(x));
+        return (int)b;
     }
 }
